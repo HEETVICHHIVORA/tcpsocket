838acb177379c784a75ddc8224053644d87f73e0
HEETVICHHIVORA <76490390+HEETVICHHIVORA@users.noreply.github.com>
2024-11-04 22:24:52 +0530

    Update tcpserver.c

--- tcpserver.c
+++ tcpserver.c
@@ -1,10 +1,11 @@
+#include <arpa/inet.h> // inet_addr()
 #include <netdb.h>
-#include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h> // bzero()
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h> // read(), write(), close()
+#include <unistd.h> 
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
