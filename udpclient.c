6df06c3e431a2603db3cb6b60f2e623c0aa7d1c6
HEETVICHHIVORA <76490390+HEETVICHHIVORA@users.noreply.github.com>
2024-11-04 22:38:23 +0530

    Update udpclient.c

--- udpclient.c
+++ udpclient.c
@@ -8,43 +8,42 @@
 #define BUFFER_SIZE 1024
 
 int main() {
-	int sockfd;
-	char buffer[BUFFER_SIZE];
-	struct sockaddr_in servaddr, cliaddr;
-	socklen_t len;
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in servaddr;
 
-	// Creating socket file descriptor
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    	perror("socket creation failed");
-    	exit(EXIT_FAILURE);
-	}
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&servaddr, 0, sizeof(servaddr));
-	memset(&cliaddr, 0, sizeof(cliaddr));
+    memset(&servaddr, 0, sizeof(servaddr));
 
-	// Filling server information
-	servaddr.sin_family = AF_INET; // IPv4
-	servaddr.sin_addr.s_addr = INADDR_ANY; // Any incoming interface
-	servaddr.sin_port = htons(PORT); // Port
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY; // Server's IP
+    servaddr.sin_port = htons(PORT); // Server's Port
 
-	// Bind the socket with the server address
-	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-    	perror("bind failed");
-    	close(sockfd);
-    	exit(EXIT_FAILURE);
-	}
+    while (1) {
+        printf("Enter message: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
 
-	len = sizeof(cliaddr); // len is value/result
+        // Send message to the server
+        sendto(sockfd, buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 
-	while (1) {
-    	int n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
-    	buffer[n] = '\0';
-    	printf("Client : %s\n", buffer);
-    	sendto(sockfd, (const char *)buffer, n, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-    	printf("Echoed message back to client\n");
-	}
+        int n, len = sizeof(servaddr);
+        n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+        buffer[n] = '\0';
+        printf("Server: %s\n", buffer);
 
-	close(sockfd);
-	return 0;
+        // Exit if user typed "exit"
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+    return 0;
 }
-
